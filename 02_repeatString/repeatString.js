87326ecbab899d7a881f274d80ffa5d357ea1c59
Jerrick Backous <77847817+JerrickBackous@users.noreply.github.com>
2023-02-23 16:39:37 -0600

    solve exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(word, repeats) {
+    if (repeats < 0) {
+        return "ERROR";
+    } else {
+        let statement = "";
+        for (i=0; i < repeats; i++) {
+            statement += word;
+        }
+        return statement;
+    }
 };
 
 // Do not edit below this line
