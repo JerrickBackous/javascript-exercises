47a5eab9252ec67365e8402062ad6c789e128fdb
Jerrick Backous <77847817+JerrickBackous@users.noreply.github.com>
2023-02-23 20:57:03 -0600

    solve exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,21 @@
-const sumAll = function() {
+const sumAll = function(numberOne, numberTwo) {
+    //assign max and min values
+    const big = Math.max(numberOne, numberTwo);
+    const small = Math.min(numberOne, numberTwo);
+    
+    //check for negative values or values with greater than one arg or non-integer
+    if ((numberOne < 0 || numberTwo < 0) || 
+        (typeof numberOne === "string" || typeof numberTwo === "string") ||
+        (numberOne.length >= 1 || numberTwo.length >= 1)) {
+            return "ERROR";
+        } else {
+            //add up values between two numbers
+            let sum = 0;
+            for (i=small; i<=big; i++) {
+                sum += i;
+            }
+            return sum;
+        }
 
 };
 
