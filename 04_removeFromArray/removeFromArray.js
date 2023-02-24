2a6254fd1b7809474c6a5087a78fcf2cbf06daf1
Jerrick Backous <77847817+JerrickBackous@users.noreply.github.com>
2023-02-23 20:40:34 -0600

    solve exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(inputs, ...cutout) {
+    //define number of cutout values
+    return inputs.filter(x => !cutout.includes(x));
 };
 
 // Do not edit below this line
