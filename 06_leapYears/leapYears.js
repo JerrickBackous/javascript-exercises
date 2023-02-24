d1a32128c67040802acaf7b9ff27d7b45a076ec0
Jerrick Backous <77847817+JerrickBackous@users.noreply.github.com>
2023-02-24 10:34:37 -0600

    solve exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    //calculate if a given year is a leap year
+    if (year % 4 === 0 && year % 400 === 0) {
+        return true;
+    } else if (year % 100 === 0) {
+        return false;
+    } else if (year % 4 === 0 ) {
+        return true;
+    } else {
+        return false;
+    }
+    //return true/false
 };
 
 // Do not edit below this line
